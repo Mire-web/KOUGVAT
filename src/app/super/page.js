81c864c01cf990ec7e0dc143d9419c2a1c5db31f
Emmanuel ifeanyichukwu <amahe8664@gmail.com>
2024-01-29 04:32:28 +0100

    Update page.js

--- src/app/super/page.js
+++ src/app/super/page.js
@@ -3,7 +3,7 @@ import React from "react";
 function Super() {
   return (
     <div>
-      <section class="bg-gray-50 dark:bg-gray-900">
+      <section class="bg-gray-50 dark:bg-gray-900 min-h-[100vh]">
         <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
           <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
             <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
@@ -26,7 +26,7 @@ function Super() {
                     type="password"
                     name="password"
                     id="password"
-                    placeholder="••••••••"
+                    placeholder="•••••"
                     class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     required
                   />
