b01674a3f05155c319dda951c865fb1a2ec2e01d
Emmanuel ifeanyichukwu <amahe8664@gmail.com>
2024-01-29 04:33:44 +0100

    Update page.js

--- src/app/(auth)/signup/page.js
+++ src/app/(auth)/signup/page.js
@@ -59,7 +59,7 @@ function SignUp() {
   }
 
   return (
-    <section className="bg-sky-50 dark:bg-sky-400">
+    <section className="bg-sky-50 dark:bg-sky-400 min-h-[100vh]">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
         <div className="w-full bg-white rounded-lg shadow-lg dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-white">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
