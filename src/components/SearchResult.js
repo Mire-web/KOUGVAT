30917747abc0877992adc41a3648d20a52ed368e
mire-web <amahe8664@gmail.com>
2024-02-04 11:26:03 +0100

    map added

--- src/components/SearchResult.js
+++ src/components/SearchResult.js
@@ -1,12 +1,14 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import InfoCard from "./InfoCard";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import { useSearchParams } from "next/navigation";
 import format from "date-fns/format";
+import MapApp from "./Map";
+import { MapIcon } from "@heroicons/react/outline";
 
-export function SearchResult(searchResult) {
+export function SearchResult({searchResult}) {
   const searchParams = useSearchParams();
   const location = searchParams.get("location");
   const startDate = format(
@@ -17,18 +19,28 @@ export function SearchResult(searchResult) {
   const noOfGuests = searchParams.get("noOfGuests");
   const formattedWord = noOfGuests > 1 ? "roommates" : "roommate";
   const range = `${startDate} - ${endDate}`;
+  const [openMap, setOpenMap] = useState(false);
   return (
     <div>
+	<div className="flex rounded-full px-4 py-2 bg-transparent text-white absolute h-[100vh] w-[100vw] border-red-800 pointer-events-none z-90">
+	  <button className="flex items-end rounded-full px-4 py-2 bg-sky-700 fixed left-1/2 bottom-5 pointer-events-auto z-[999] outline-none" onClick={()=>setOpenMap(!openMap)}>
+		<MapIcon className="mr-2 h-6" />
+		Map
+	  </button>
+	</div>
       <Header
-        placeholder={`${location || 'any'} | ${range || 'any'} | ${noOfGuests || 'any'} ${formattedWord}`}
+        placeholder={`${location || "any"} | ${range || "any"} | ${
+          noOfGuests || "any"
+        } ${formattedWord}`}
       />
       <main className="flex">
+        <div className={`${!openMap && 'hidden'} fixed top-5 left-0 overflow-hidden min-h-[100vh] z-[998] min-w-[100vw]`}>
+          <MapApp searchResult={searchResult}/>
+        </div>
         <section className="flex-grow pt-14 px-6">
-          <p className="text-xs">
-            Lodges - {range}
-          </p>
+          <p className="text-xs">Lodges - {range}</p>
           <h1 className="text-3xl font-semibold mt-2 mb-6">
-            Availiable in {location || 'Any location'}
+            Availiable in {location || "Any location"}
           </h1>
           <div className="hidden lg:inline-flex mb-5 space-x-3 text-gray-800 whitespace-nowrap">
             <p className="button">Water</p>
@@ -37,7 +49,7 @@ export function SearchResult(searchResult) {
             <p className="button">Bungalow</p>
             <p className="button">Storey Building</p>
           </div>
-          {searchResult.searchResult.map((item) => (
+          {searchResult.map((item) => (
             <InfoCard
               key={item.img}
               img={item.img}
@@ -47,9 +59,10 @@ export function SearchResult(searchResult) {
               star={item.star}
               price={item.price}
             />
-		  ))}
+          ))}
         </section>
       </main>
+
       <Footer />
     </div>
   );
